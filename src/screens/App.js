cca6b454f53d320cc94fa307f19f72dbb1ae4397
Wiktor Kukulski <laxytt@gmail.com>
2020-02-21 20:37:19 +0100

    initial commit

---
+++ src/screens/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../components/SearchBar";
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+      <h1>Companies table will be here hehe</h1>
+    </div>
+  );
+};
+
+export default App;
